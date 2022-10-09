8fb23c64f7660123ff0e932c96b3ec0a088053e6
Johan Lindell <johan@lindell.me>
2022-10-09 08:54:14 +0000

    Added deploy script

--- build.js
+++ build.js
@@ -2,6 +2,10 @@ const resume = require("./resume/resume.json");
 const resumeGenerator = require("jsonresume-theme-paper-plus-plus");
 const minify = require("html-minifier").minify;
 const fs = require("fs");
+const fse = require("fs-extra");
+const path = require("path");
+
+const buildDir = "build";
 
 const html = resumeGenerator.render(resume);
 const minified = minify(html, {
@@ -10,4 +14,9 @@ const minified = minify(html, {
   minifyJS: true,
 });
 
-fs.writeFileSync("./index.html", minified);
+fse.removeSync(buildDir);
+fs.mkdirSync(buildDir);
+
+fs.writeFileSync(path.join(buildDir, "index.html"), minified);
+fse.copySync("images", path.join(buildDir, "images"));
+fse.copyFileSync("CNAME", path.join(buildDir, "CNAME"));
